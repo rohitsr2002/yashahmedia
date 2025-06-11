a3c4ab1b1deb689174d04264fd242dbe63b0b380
Manish Raj <120162412+raj13manish@users.noreply.github.com>
2025-06-11 12:01:48 +0530

    h chnges

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <header>
             <h1>Header Component</h1>
             <h2>Welcome to My Website</h2>
-          
+            <h4>Enjoy your stay!</h4>
         </header>
         
     );
